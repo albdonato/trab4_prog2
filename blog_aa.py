e288a8dce8bf4a50ef8863fdf6cefb05119bd292
Alberto Donato <58197542+albdonato@users.noreply.github.com>
2021-04-12 11:16:44 -0300

    Add files via upload

---
+++ blog_aa.py
@@ -0,0 +1,48 @@
+#Trabalho de progamação aplicada 2 
+##################################
+#Professor: Jefferson Santos
+# Dupla: Alberto Pinheiro Filho; Ana Carolina Do Valle
+#1.Importando as bibliotecas necessárias para executar o programa
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
+blog_aa = Flask(__name__)
+
+blog_aa.config["SESSION_PERMANENT"] = False
+blog_aa.config["SESSION_TYPE"] = "filesystem"
+
+Session(blog_aa)
+
+@blog_aa.route("/", methods=["GET", "POST"])
+def index():
+    if session.get("notes") is None:
+        session["notes"] = []
+
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+        
+    return render_template("index.html", notes=session["notes"])
+
+@blog_aa.route("/new_post", methods=["GET", "POST","DEL"])
+def new_post():
+    if session.get("notes") is None:
+        session["notes"] = []
+
+    if session.get("title") is None:
+        session["titles"] = []
+
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+        
+    if request.method == "POST":
+        title = request.form.get("title")
+        session["titles"].append(title)
+
+    return render_template ("new_post.html", notes=session["notes"], titles=session["titles"])
+
+#@blog_aa.route(f"/{note}", methods=["POST","GET"])
+#def edit(note):
+
+
