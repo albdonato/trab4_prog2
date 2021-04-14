e76119299c00f2dcd34186878cfbb046e8802c6f
Alberto Donato <58197542+albdonato@users.noreply.github.com>
2021-04-13 21:56:11 -0300

    Update blog_aa.py

--- blog_aa.py
+++ blog_aa.py
@@ -15,33 +15,31 @@ Session(blog_aa)
 
 @blog_aa.route("/", methods=["GET", "POST"])
 def index():
-    if session.get("notes") is None:
+    if  session.get("notes") is None:
         session["notes"] = []
 
     if request.method == "POST":
+        title = request.form.get("title")
+        session["notes"].append(title)
         note = request.form.get("note")
         session["notes"].append(note)
-        
+
+
     return render_template("index.html", notes=session["notes"])
 
 @blog_aa.route("/new_post", methods=["GET", "POST","DEL"])
 def new_post():
-    if session.get("notes") is None:
-        session["notes"] = []
-
-    if session.get("title") is None:
-        session["titles"] = []
+    if  session.get("notes") is None:
+            session["notes"] = []
 
     if request.method == "POST":
-        note = request.form.get("note")
-        session["notes"].append(note)
-        
-    if request.method == "POST":
-        title = request.form.get("title")
-        session["titles"].append(title)
+            title = request.form.get("title")
+            session["notes"].append(title)
+            note = request.form.get("note")
+            session["notes"].append(note)
+            
 
-    return render_template ("new_post.html", notes=session["notes"], titles=session["titles"])
-
+    return render_template("new_post.html", notes=session["notes"])
 #@blog_aa.route(f"/{note}", methods=["POST","GET"])
 #def edit(note):
 
