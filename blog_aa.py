ba7e524a7c0a399f748f67cf4cc0306c6d24cb1e
Alberto Donato <58197542+albdonato@users.noreply.github.com>
2021-04-14 20:15:59 -0300

    Update blog_aa.py

--- blog_aa.py
+++ blog_aa.py
@@ -13,30 +13,36 @@ blog_aa.config["SESSION_TYPE"] = "filesystem"
 
 Session(blog_aa)
 
-@blog_aa.route("/", methods=["GET", "POST"])
+@blog_aa.route('/', methods=['POST', 'GET'])
 def index():
-    if  session.get("notes") is None:
-        session["notes"] = []
-
-    if request.method == "POST":
-        title = request.form.get("title")
-        note = request.form.get("note")
-        blog_post=[title,note]
-        session["notes"].append(blog_post)
-
-
-    return render_template("index.html", notes=session["notes"])
-
-@blog_aa.route("/new_post", methods=["GET", "POST","DEL"])
-def new_post():
-    if  session.get("notes") is None:
-            session["notes"] = []
-
-    if request.method == "POST":
-        title = request.form.get("title")
-        note = request.form.get("note")
-        blog_post=[title,note]
-        session["notes"].append(blog_post)
+    if session.get('notes') is None:
+        session['notes']=[]
     
-    if request.method == "GET":
+    if request.method=='POST':
+        title=request.form.get('title')
+        message=request.form.get('message')
+        if session['notes']==[]:
+            id=1
+        else:
+            id= (session['notes'][-1]).get('id')+1
+        post = {'title':title,'message':message,'id':id}
+        session['notes'].append(post)
+    return render_template('index.html', notes=session['notes'])
 
+
+    
+
+@blog_aa.route('/new-post', methods=['GET', 'POST'])
+def new_post():
+    return render_template('new_post.html')
+
+@blog_aa.route('/edit-<int:id>', methods=['GET','POST'])
+def edit_post(id):
+    input_note={}
+    notes=session['notes']
+    for note in notes:
+        if note.get('id')==id:
+            title=note.get('title')
+            message=note.get('message')
+            input_note={'title':title,'message':message,'id':id}
+    return render_template('new_post.html', input_note=input_note, notes=session['notes'])
